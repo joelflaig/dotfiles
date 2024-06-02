477e006570b23fadd0fa069e4bb1d1c08d3cc4b3
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-06-02 22:13:03 +0200

    nvim: Changed " " in commit messages to "<leader>", changed gitsign for topdelete.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,17 +7,20 @@ local telescope = require('telescope.builtin')
 
 -- basic mappings
 map("n", ";", ":", { desc = " CMD enter command mode" })
-map("n", " d", "<cmd> Dashboard <cr>", { desc = "󰕮 Display Dashboard" })
+map("n", "<leader>d", "<cmd> Dashboard <cr>", { desc = "󰕮 Display Dashboard" })
 -- map("i", "jk", "<ESC>")
 -- map("i", "<Enter>", "<CR>")
-map("n", " q", "<cmd> qa <cr>", { desc = " Quit all open buffers"})
-map("n", " w", "<cmd> wa <cr>", { desc = " Save"})
-map("n", " wq", "<cmd> wqa <cr>", { desc = " Save"})
-map("n", " l", "<cmd> Lazy <cr>", { desc = "󰒲 Open Lazy" })
+map("n", "<leader>q", "<cmd> qa <cr>", { desc = " Quit all open buffers"})
+map("n", "<leader>w", "<cmd> wa <cr>", { desc = " Save"})
+map("n", "<leader>wq", "<cmd> wqa <cr>", { desc = " Save"})
+map("n", "<leader>l", "<cmd> Lazy <cr>", { desc = "󰒲 Open Lazy" })
 
 -- telescope mappings
 map('n', '<leader>ff', telescope.find_files, {}, { desc = " Find files" })
 map('n', '<leader>fg', telescope.live_grep, {}, { desc = " Find text" })
 map('n', '<leader>fb', telescope.buffers, {}, { desc = " Find buffers" })
 map('n', '<leader>fh', telescope.help_tags, {}, { desc = " Find help" })
+
+-- gitsigns
+map('n', '<leader>tlb', '<cmd> Gitsigns toggle_current_line_blame <cr>', { desc = "Toggle git blame" })
 
