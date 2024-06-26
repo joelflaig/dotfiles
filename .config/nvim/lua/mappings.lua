a20a5756f734a080de2c72c3564c2b55cf5cf63e
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-06-26 22:48:02 +0200

    nvim: Added code snapshot plugin, telescope mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,8 +5,9 @@ local unmap = vim.keymap.del
 -- telescope
 local telescope = require('telescope.builtin')
 
+
 -- basic mappings
-map("n", ";", ":", { desc = " CMD enter command mode" })
+map({ "n", "v" }, ";", ":", { desc = " CMD enter command mode" })
 map("n", "<leader>dd", "<cmd> Dashboard <cr>", { desc = "󰕮 Display Dashboard" })
 -- map("i", "jk", "<ESC>")
 -- map("i", "<Enter>", "<CR>")
@@ -30,11 +31,12 @@ map('n', "<M-k>", "<C-w>-")
 map('n', "<M-l>", "<C-w>>")
 
 -- telescope mappings
-map('n', '<leader>tel', '<cmd> Telescope <cr>', {}, { desc = "Open telescope" })
-map('n', '<leader>ff', telescope.find_files, {}, { desc = " Find files" })
-map('n', '<leader>fg', telescope.live_grep, {}, { desc = " Find text" })
-map('n', '<leader>fb', telescope.buffers, {}, { desc = " Find buffers" })
-map('n', '<leader>fh', telescope.help_tags, {}, { desc = " Find help" })
+map('n', '<leader>tel', '<cmd> Telescope <cr>', { desc = "Open telescope" })
+map('n', '<leader>ff', telescope.find_files, { desc = " Find files" })
+map('n', '<leader>fg', telescope.live_grep, { desc = " Find text" })
+map('n', '<leader>fb', telescope.buffers, { desc = " Find buffers" })
+map('n', '<leader>fh', telescope.help_tags, { desc = " Find help" })
+map('n', '<leader>fr', telescope.oldfiles, { desc = "󰑐 Find old files"})
 
 -- gitsigns
 map('n', '<leader>tlb', '<cmd> Gitsigns toggle_current_line_blame <cr>', { desc = "Toggle git blame" })
---
+++ .config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,15 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make",
+  keys = {
+    { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "v", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "v", desc = "Save selected code snapshot in ~/Pictures" },
+  },
+  opts = {
+    save_path = "~/Pictures",
+    has_breadcrumbs = true,
+    bg_theme = "default",
+    watermark = "Joel Flaig",
+    mac_window_bar = true,
+  },
+}
