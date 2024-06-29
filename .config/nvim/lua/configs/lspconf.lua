f8092531727e5cfab3b953d440a67d85da733708
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-06-29 23:39:59 +0200

    nvim: Renamed lspconfig.lua

--- .config/nvim/lua/configs/lspconf.lua
+++ .config/nvim/lua/configs/lspconf.lua
@@ -41,6 +41,12 @@ lspconfig.tsserver.setup {
   capabilities = capabilities,
 }
 
+-- diagnostic signs
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError", numhl = "" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn", numhl = "" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo", numhl = "" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌶 ", texthl = "DiagnosticSignHint", numhl = "" })
+
 local cmp = require 'cmp'
 
 cmp.setup{
@@ -91,4 +97,6 @@ cmp.setup{
     matching = { disallow_symbol_nonprefix_matching = false }
   }),
 }
+
+return {}
 
