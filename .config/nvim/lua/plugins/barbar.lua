c66a48ae20f77470586eb9df77e7bd7010e63213
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-06-30 17:47:22 +0200

    nvim: Customized barbar.nvim

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -1,10 +1,29 @@
 return {
   'romgrk/barbar.nvim',
+  lazy = false,
   dependencies = {
     'lewis6991/gitsigns.nvim',
     'nvim-tree/nvim-web-devicons',
   },
-  init = function() vim.g.barbar_auto_setup = false end,
+  init = function()
+    vim.g.barbar_auto_setup = false
+    -- highlights
+    local bg = "#1c1c1c"
+    local fg = "#767676"
+    local cfg = "#87afff"
+    vim.api.nvim_set_hl(0, "BufferCurrent", { bg = bg, fg = cfg })
+    vim.api.nvim_set_hl(0, "BufferAlternate", { bg = bg, fg = fg })
+    vim.api.nvim_set_hl(0, "BufferInactive", { bg = bg, fg = fg })
+    vim.api.nvim_set_hl(0, "BufferVisible", { bg = bg, fg = fg })
+    vim.api.nvim_set_hl(0, "BufferCurrentMod", { bg = bg, fg = cfg })
+    vim.api.nvim_set_hl(0, "BufferAlternateMod", { bg = bg, fg = fg })
+    vim.api.nvim_set_hl(0, "BufferInactiveMod", { bg = bg, fg = fg })
+    vim.api.nvim_set_hl(0, "BufferVisibleMod", { bg = bg, fg = fg })
+    vim.api.nvim_set_hl(0, "BufferCurrentSign", {fg = bg})
+    vim.api.nvim_set_hl(0, "BufferAlternateSign", {fg =bg})
+    vim.api.nvim_set_hl(0, "BufferInactiveSign", {fg = bg})
+    vim.api.nvim_set_hl(0, "BufferVisibleSign", {fg = bg})
+  end,
   opts = {
     animation = true,
     auto_hide = false,
@@ -14,36 +33,36 @@ return {
     hide = {extensions = false, inactive = false},
     highlight_alternate = false,
     highlight_inactive_file_icons = false,
-    highlight_visible = true,
+    highlight_visible = false,
 
     icons = {
       buffer_index = false,
       buffer_number = false,
       button = '',
       diagnostics = {
-        [vim.diagnostic.severity.ERROR] = {enabled = true, icon = ' '},
-        [vim.diagnostic.severity.WARN] = {enabled = true, icon = ' '},
-        [vim.diagnostic.severity.INFO] = {enabled = true, icon = ' '},
-        [vim.diagnostic.severity.HINT] = {enabled = true, icon = '󰌶 '},
+        [vim.diagnostic.severity.ERROR] = {enabled = false, icon = ' '},
+        [vim.diagnostic.severity.WARN] = {enabled = false, icon = ' '},
+        [vim.diagnostic.severity.INFO] = {enabled = false, icon = ' '},
+        [vim.diagnostic.severity.HINT] = {enabled = false, icon = '󰌶 '},
       },
       gitsigns = {
-        added = {enabled = true, icon = ' '},
-        changed = {enabled = true, icon = ' '},
-        deleted = {enabled = true, icon = ' '},
+        added = {enabled = false, icon = ' '},
+        changed = {enabled = false, icon = ' '},
+        deleted = {enabled = false, icon = ' '},
       },
       filetype = {
         custom_colors = false,
         enabled = true,
       },
-      separator = {left = '▎', right = ''},
-      separator_at_end = true,
+      separator = {left = ' ', right = ' '},
+      separator_at_end = false,
       modified = {button = '●'},
       pinned = {button = '', filename = true},
       preset = 'default',
-      alternate = {filetype = {enabled = false}},
-      current = {buffer_index = true},
-      inactive = {button = '×'},
-      visible = {modified = {buffer_number = false}},
+      alternate = {separator = {left = '', right = ''}, filetype = {enabled = false}},
+      current = {separator = {left = '', right = ''}, buffer_index = false},
+      inactive = {separator = {left = '', right = ''}, button = '×'},
+      visible = {separator = {left = '', right = ''}, modified = {buffer_number = false}},
     },
     insert_at_end = false,
     insert_at_start = false,
