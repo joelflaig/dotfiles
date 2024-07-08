8f454112e8fc8cf2937e2c6599d45f35b8db3a13
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-07-08 22:47:16 +0200

    nvim: Fixed outline width and tabline colors

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -8,18 +8,19 @@ return {
   init = function()
     vim.g.barbar_auto_setup = false
     -- highlights
-    local bg = "#262626"
+    local cbg = "#222335"
+    local cfg = "#89b4fa"
+    local bg = "#1e1e2e"
     local fg = "#767676"
-    local cfg = "#87afff"
-    vim.api.nvim_set_hl(0, "BufferCurrent", { bg = bg, fg = cfg })
+    vim.api.nvim_set_hl(0, "BufferCurrent", { bg = cbg, fg = cfg })
     vim.api.nvim_set_hl(0, "BufferAlternate", { bg = bg, fg = fg })
     vim.api.nvim_set_hl(0, "BufferInactive", { bg = bg, fg = fg })
     vim.api.nvim_set_hl(0, "BufferVisible", { bg = bg, fg = fg })
-    vim.api.nvim_set_hl(0, "BufferCurrentMod", { bg = bg, fg = cfg })
+    vim.api.nvim_set_hl(0, "BufferCurrentMod", { bg = cbg, fg = cfg })
     vim.api.nvim_set_hl(0, "BufferAlternateMod", { bg = bg, fg = fg })
     vim.api.nvim_set_hl(0, "BufferInactiveMod", { bg = bg, fg = fg })
     vim.api.nvim_set_hl(0, "BufferVisibleMod", { bg = bg, fg = fg })
-    vim.api.nvim_set_hl(0, "BufferCurrentSign", {fg = bg})
+    vim.api.nvim_set_hl(0, "BufferCurrentSign", {fg = cbg})
     vim.api.nvim_set_hl(0, "BufferAlternateSign", {fg = bg})
     vim.api.nvim_set_hl(0, "BufferInactiveSign", {fg = bg})
     vim.api.nvim_set_hl(0, "BufferVisibleSign", {fg = bg})
@@ -78,7 +79,7 @@ return {
         align = 'center',
       },
       ['neo-tree'] = {event = 'BufWipeout'},
-      Outline = {event = 'BufWinLeave', text = 'symbols-outline', align = 'right'},
+      Outline = {event = 'BufWinLeave', text = 'symbols-outline', align = 'center'},
     },
 
     letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
--- .config/nvim/lua/plugins/symbols-outline.lua
+++ .config/nvim/lua/plugins/symbols-outline.lua
@@ -7,7 +7,7 @@ return {
     auto_preview = false,
     position = 'right',
     relative_width = true,
-    width = 30,
+    width = 25,
     auto_close = false,
     show_numbers = false,
     show_relative_numbers = false,
