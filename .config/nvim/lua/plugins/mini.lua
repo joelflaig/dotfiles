fff80a1a1efc0aebcc41618503d33feef28bfdef
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-07-22 22:23:28 +0200

    nvim: Added minimap for no reason at all, mini.animate

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,4 +1,7 @@
 return {
   "echasnovski/mini.nvim",
   lazy = false,
+  config = function ()
+    require("mini.animate").setup({})
+  end
 }
---
+++ .config/nvim/lua/plugins/minimap.lua
@@ -0,0 +1,27 @@
+return {
+  "Isrothy/neominimap.nvim",
+  enabled = true,
+  lazy = false, -- WARN: NO NEED to Lazy load
+  init = function()
+    vim.opt.wrap = false -- Recommended
+    vim.opt.sidescrolloff = 36 -- It's recommended to set a large value
+    vim.g.neominimap = {
+      auto_enable = true,
+      window_border = "rounded",
+
+      exclude_filetypes = {
+        "help",
+        "dashboard",
+      },
+
+      exclude_buftypes = {
+        "nofile",
+        "nowrite",
+        "quickfix",
+        "terminal",
+        "prompt",
+      },
+    }
+  end,
+}
+
