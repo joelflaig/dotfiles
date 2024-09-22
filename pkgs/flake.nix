809b5f5d406a6536ede7b9a4bd906528dd9b6fba
Mathprogrammer5 <j.flaig@progymnasium-alpirsbach.de>
2024-09-22 22:12:01 +0200

    pkgs: Added a whole *ss lot of packages

--- pkgs/flake.nix
+++ pkgs/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My System Configuration";
+  description = "My System Configuration";          
 
   outputs = { self, nixpkgs }:
     let
@@ -30,6 +30,7 @@
           lua # lua -- lua interpreter
           nodejs_22 # nodejs -- javascript
           rustup # rustup -- rust language
+          go # go -- golang
 
           # editing
           neovim # neovim -- editor
@@ -60,10 +61,51 @@
 
           # web
           firefox # firefox -- web browser
+
+          # notes
+          obsidian # obsidian -- note taking
+
+          # recording
+          obs-studio # obs studio -- screen recording
+
+          # notifications
+          dunst # dunst -- notification daemon
+          libnotify # libnotify -- notify-send
+
+          # authentication
+          kdePackages.polkit-kde-agent-1
+
+          # discord -- chat
+          discord
+
           # clipboard
           wl-clipboard # wl-clipboard -- clipboard manager
           wl-clip-persist # wl-clip-persist -- keep wl-clipboard after program closes
           clipse # clipse -- tui clipboard manager
+
+          # automount
+          udiskie # udiskie -- automount usb drives
+
+          # hyprland
+          hyprpicker # hyprpicker -- color picker
+
+          # zip
+          unzip # unzip -- unzip tool
+
+          # fetch
+          neofetch # neofetch -- system information tool
+
+          # clock
+          gnome-clocks # gnome-clocks -- clock
+
+          # speech
+          speechd
+
+          # images
+          loupe # loupe -- image viewer
+
+          # wallpaper
+          swww # swww -- wallpaper manager
         ];
       };
     };
